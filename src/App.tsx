4411d99007a2980ca0486bb15d8454b796dda726
Camila Serantoni <camila.serantoni@blue.cl>
2024-11-25 08:14:16 -0300

    excercise header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import CourseGoal from "./components/CourseGoal.tsx";
-
+import GoalsImg from "./assets/goals.jpg";
+import Header from "./components/Header.tsx";
 export default function App() {
   return (
     <main>
+      <Header image={{src: GoalsImg, alt: "Goals" }}>
+        <h1>Course Goals</h1>
+      </Header>
       <CourseGoal title="Learn React + TS">Learn it from the ground up</CourseGoal>
     </main>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+
+
+type HeaderProps = {
+    children:ReactNode;
+    image:{src:string, alt:string}
+}
+
+export default function Header({ image, children }: HeaderProps) {
+    return (
+      <header>
+        <img {...image} />
+        {children}
+      </header>
+    );
+  }
