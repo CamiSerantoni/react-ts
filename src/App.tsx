47dc642d67610d7f08f43e2c194e6f99dc02b8ab
Camila Serantoni <camila.serantoni@blue.cl>
2024-11-27 23:54:01 -0300

    working with status

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,43 @@ import { useState } from "react";
 
 type CourseGoal = {
   title: string;
-  descriptions: string; 
+  description: string;
   id: number;
-}
+};
 
 export default function App() {
-
-const [goals, setGoals] = useState<CourseGoal[]>([]);
-function handleAddGoal(){
-
-}
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "learn React + TS",
+        description: "Learn it from the ground up",
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
 
   return (
     <main>
-      <Header image={{src: GoalsImg, alt: "Goals" }}>
+      <Header image={{ src: GoalsImg, alt: "Goals" }}>
         <h1>Course Goals</h1>
       </Header>
       <button onClick={handleAddGoal}>Add Goal </button>
-      <CourseGoal title="Learn React + TS">Learn it from the ground up</CourseGoal>
+      <ul>
+        {goals.map((goal) => (
+          <li>
+            <CourseGoal key={goal.id} title={goal.title}>
+              {" "}
+              <p>learn it from the ground up</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+
+      <CourseGoal title="Learn React + TS">
+        Learn it from the ground up
+      </CourseGoal>
     </main>
   );
 }
