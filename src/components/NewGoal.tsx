469a4cf44ac16ff1896dd0200b692315b610e711
Camila Serantoni <camila.serantoni@blue.cl>
2024-12-02 23:34:00 -0300

    adding  new component

---
+++ src/components/NewGoal.tsx
@@ -0,0 +1,21 @@
+export default function NewGoal() {
+
+function  handleSubmit () {
+
+}
+
+
+    return <form onSubmit={handleSubmit}>
+        <p>
+            <label htmlFor="goal">Your Goal</label>
+            <input id="goal" type="text" />
+        </p>
+        <p>
+            <label htmlFor="summary">Short Summary </label>
+            <input id="summary" type="text" />
+        </p>
+        <p>
+            <button>Add Goal </button>
+        </p>
+    </form>
+}
